d53962929e4df5d87201cc0a338183ddb05b71f4
NABrainn <michal2002.60@gmail.com>
2023-10-25 13:41:08 +0200

    fix the issue where container does not appear as a square at smaller grid quantities eg. 1, remove borders from the grids

--- etch-a-sketch-script.js
+++ etch-a-sketch-script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 const button = document.querySelector('button');
+let containerWidth = 0;
 let containerSize;
 
 button.addEventListener('click', () => {
@@ -8,18 +9,23 @@ button.addEventListener('click', () => {
     }while(containerSize <= 0 || containerSize > 100 || isNaN(containerSize) === true);
 
     container.innerHTML = "";
+    containerWidth = 0;
 
     for(let i = 0; i < containerSize; i++) {
         let row = document.createElement("div");
         row.setAttribute("class", "rows");
-        container.append(row);    
-        for(let j = 0; j < containerSize; j++) {
+        container.append(row);
+        if(containerWidth <= 700) { 
+            containerWidth += 50; 
+        }
+        for(let j = 0; j < containerSize; j++) {           
+            container.style.maxWidth = `${containerWidth}px`;
             let grid = document.createElement("div");
             grid.setAttribute('class', 'grids');
             row.append(grid);        
         }
     }
-    
+    container.style.border = "2px solid black";
     let firstColor = Math.floor(Math.random() * 254);
     let secondColor = Math.floor(Math.random() * 254);
     let thirdColor = Math.floor(Math.random() * 254);
