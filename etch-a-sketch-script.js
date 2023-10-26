e614d29a1f39596ded345b9b11038f8a9baca764
NABrainn <michal2002.60@gmail.com>
2023-10-26 08:52:31 +0200

    add 10x10 default board

--- etch-a-sketch-script.js
+++ etch-a-sketch-script.js
@@ -2,6 +2,36 @@ const container = document.getElementById('container');
 const button = document.querySelector('button');
 let containerSize;
 
+function generateGrids(length) {
+    for(let i = 0; i < length; i++) {
+        let row = document.createElement("div");
+        row.setAttribute("class", "rows");
+        container.append(row);
+        for(let j = 0; j < length; j++) {           
+            let grid = document.createElement("div");
+            grid.setAttribute('class', 'grids');
+            row.append(grid);        
+        }
+    }
+}
+
+function changeGridColorOnMouseOver() {
+    document.querySelectorAll('#container > .rows > .grids').forEach((grid) => { 
+        let brightnessOffset = 100;          
+        grid.addEventListener('mouseover', () => {
+            let firstColor = Math.floor(Math.random() * 254);
+            let secondColor = Math.floor(Math.random() * 254);
+            let thirdColor = Math.floor(Math.random() * 254);               
+            grid.style.backgroundColor = `rgb(${firstColor}, ${secondColor}, ${thirdColor})`;
+            grid.style.filter = `brightness(${brightnessOffset}%)`;
+            brightnessOffset -= 10;
+        })
+    })
+}
+
+const defaultContainer = generateGrids(10);
+changeGridColorOnMouseOver();
+
 button.addEventListener('click', () => {
     do {
         containerSize = parseInt(prompt("Enter a number in range (0, 100>"));
@@ -9,32 +39,12 @@ button.addEventListener('click', () => {
 
     container.innerHTML = "";
 
-    for(let i = 0; i < containerSize; i++) {
-        let row = document.createElement("div");
-        row.setAttribute("class", "rows");
-        container.append(row);
-        for(let j = 0; j < containerSize; j++) {           
-            let grid = document.createElement("div");
-            grid.setAttribute('class', 'grids');
-            row.append(grid);        
-        }
-    }
+    generateGrids(containerSize);
+
     container.style.maxWidth = '700px';
     container.style.maxWidth = '700px';
-    container.style.border = "2px solid black";
     
-    document.querySelectorAll('#container > .rows > .grids').forEach((grid) => { 
-        let brightnessOffset = 100;             
-        grid.addEventListener('mouseover', () => {
-            let firstColor = Math.floor(Math.random() * 254);
-            let secondColor = Math.floor(Math.random() * 254);
-            let thirdColor = Math.floor(Math.random() * 254);
-            
-            grid.style.backgroundColor = `rgb(${firstColor}, ${secondColor}, ${thirdColor})`;
-            grid.style.filter = `brightness(${brightnessOffset}%)`;
-            brightnessOffset -= 10;
-        })
-    })
+    changeGridColorOnMouseOver();
 })
 
 
